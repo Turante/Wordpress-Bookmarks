b0b78c253d0fc7e9a4a3fa7b1abe936c11998f2e
Aaron Parecki <aaron@parecki.com>
2011-04-11 09:27:17 -0700

    Adds a "description" field for adding arbitrary text to bookmarks.

--- bookmarks.php
+++ bookmarks.php
@@ -39,7 +39,7 @@ function pk_create_bookmark_post_type() {
 		'capability_type'=>'post',
     	'supports'=>array('title','thumbnail'),
     	'taxonomies'=>array('category', 'post_tag'),
-		'slug'=>'email', 
+		'slug'=>'bookmark',
 		'hierarchical'=>false, 
 		'menu_position'=>4
 	));
@@ -49,18 +49,21 @@ function pk_save_bookmark() {
     global $post;
     if ($_POST['pk_bookmark_link_url'])
         update_post_meta($post->ID, 'link_url', $_POST['pk_bookmark_link_url'], false);
+    if ($_POST['pk_bookmark_link_desc'])
+        update_post_meta($post->ID, 'link_desc', $_POST['pk_bookmark_link_desc'], false);
 }
 
 function pk_bookmark_add_meta_boxes() {
-    add_meta_box('pk_bookmark_headers', 'URL', 'pk_bookmark_metabox', 'bookmark', 'normal', 'high');
+    add_meta_box('pk_bookmark_headers', 'Bookmark', 'pk_bookmark_metabox', 'bookmark', 'normal', 'high');
 }
 
 function pk_bookmark_metabox($post) {
     $custom = get_post_custom($post->ID);
     
-    echo '<table>';
-	    echo '<tr><td><b>URL:</b></td><td><input type="text" name="pk_bookmark_link_url" value="'.($custom ? htmlspecialchars(array_pop($custom['link_url'])) : '').'" style="width: 400px"/></td></tr>';
-    echo '</table>';
+    echo '<table width="100%">';
+	    echo '<tr><td width="110"><b>URL:</b></td><td><input type="text" name="pk_bookmark_link_url" value="'.($custom && $custom['link_url'] ? htmlspecialchars(array_pop($custom['link_url'])) : '').'" style="width: 100%"/></td></tr>';
+	    echo '<tr><td><b>Description:</b></td><td><input type="text" name="pk_bookmark_link_desc" value="'.($custom && $custom['link_desc'] ? htmlspecialchars(array_pop($custom['link_desc'])) : '').'" style="width: 100%"/></td></tr>';
+	echo '</table>';
     
 ?>
 <script type="text/javascript">
